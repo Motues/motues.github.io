bb20964a963caf0aedc5c19ab5ffe49182cdfd98
Motues <2901987286@qq.com>
2024-07-10 23:20:21 +0800

    V2.0

---
+++ src/content/blog/first-page.md
@@ -0,0 +1,16 @@
+---
+title: 第一篇文章
+author: Motues
+pubDatetime: 2024-07-10T12:14:00Z
+slug: my-first-blog
+featured: true
+draft: false
+tags:
+  - 其他
+ogImage: ""
+description: Motues的第一篇文章。
+canonicalURL: https://example.org/my-article-was-already-posted-here
+---
+
+这是我的第一篇Blog！
+![something](/image/my-first-post/01.jpg)
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import type socialIcons from "@assets/socialIcons";
+
+export type Site = {
+  website: string;
+  author: string;
+  desc: string;
+  title: string;
+  ogImage?: string;
+  lightAndDarkMode: boolean;
+  postPerPage: number;
+  scheduledPostMargin: number;
+  sharelink: boolean;
+};
+
+export type SocialObjects = {
+  name: keyof typeof socialIcons;
+  href: string;
+  active: boolean;
+  linkTitle: string;
+}[];
