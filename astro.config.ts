78e5bb2668aa7d9096beb6741872dfaedacd179b
Motues <2901987286@qq.com>
2024-07-13 22:20:35 +0800

    m

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { shield } from "@kindspells/astro-shield";
 
 import { SITE } from "./src/config";
 
@@ -21,7 +20,6 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
-    shield({}),
   ],
   markdown: {
     remarkPlugins: [
