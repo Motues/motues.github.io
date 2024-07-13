ccee4c9a4f13c2d530ae5d53feb1aa79dd2f4d8d
Motues <2901987286@qq.com>
2024-07-13 21:39:54 +0800

    V2.01

---
+++ src/config.ts
@@ -0,0 +1,161 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://motues.top", // replace this with your deployed domain
+  author: "Motues",
+  desc: "Motues's Blog",
+  title: "Motues",
+  ogImage: "og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  sharelink: false,
+};
+
+export const LOCALE = {
+  lang: "zh", // html lang code. Set this empty and default will be "en"
+  langTag: ["zh-CN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: false,
+  width: 200,
+  height: 40,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/motues",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: false,
+  },
+  {
+    name: "Mail",
+    href: "mailto:2901987286@qq.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: false,
+  },
+  {
+    name: "Twitch",
+    href: "",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+  {
+    name: "QQ",
+    href: "tencent://message/?uin=2901987286&Site=&Menu=yes",
+    linkTitle: `Send me a message on QQ`,
+    active: true,
+  },
+  {
+    name: "Phone",
+    href: "tel:19856013376",
+    linkTitle: `Call ${SITE.title}`,
+    active: true,
+  },
+  
+];
---
+++ src/content/blog/blog-temple.md
@@ -0,0 +1,45 @@
+---
+title: Blog模板
+author: Motues
+pubDatetime: 2024-07-11T17:54:00Z
+slug: blog-temple
+featured: false
+draft: true
+tags:
+  - 其他
+ogImage: ""
+description: 这是一篇模板，方便快速创建Blog。
+canonicalURL: https://motues.top/posts
+---
+
+这个是Blog模板，方便快速创建Blog。
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
+
+1
