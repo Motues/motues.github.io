5b470a13b506bde2efe2bd31f8bdd7ae826aa455
Motues <2901987286@qq.com>
2025-08-15 23:51:24 +0800

    更新网站副标题

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Motues',
-  subtitle: "Motues's Blog",
+  subtitle: "记录生活",
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
