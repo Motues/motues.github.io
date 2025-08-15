6f99323b8332a2030f94e1650848b8a8a0b33782
Motues <2901987286@qq.com>
2025-08-15 23:59:11 +0800

    更新网站配置
    - 将 site URL 从 https://motues.top/ 修改为 https://github.motues.top/

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://motues.top/',
+  site: 'https://github.motues.top/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
