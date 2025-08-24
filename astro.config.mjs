36b451407ad8cd88b75bdf080754bb8700bd53a5
Motues <2901987286@qq.com>
2025-08-24 15:14:30 +0800

    更新网站 URL 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.motues.top/',
+  site: 'https://blog.motues.top/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
